77ad5ea5b25cc48de0f3d7462d63af938d1522ea
EzePze <taylorjay098@gmail.com>
2022-07-26 16:54:53 +1000

    added mint, burn support

--- src/subscribe_test.py
+++ src/subscribe_test.py
@@ -45,19 +45,38 @@ def convert_dict_to_hexbytes(dict_):
             convert_list_to_hexbytes(dict_[key])
     return dict_
 
-def process_swap_log(contract, log):
-    hex_log = convert_dict_to_hexbytes(log.copy())
+def process_swap_log(contract, hex_log):
     processed_log = contract.events.Swap().processLog(hex_log)
     return processed_log
 
-async def subscribe_to_swap_logs(ws, pair, msg):
+def process_mint_log(contract, hex_log):
+    processed_log = contract.events.Mint().processLog(hex_log)
+    return processed_log
+
+def process_burn_log(contract, hex_log):
+    processed_log = contract.events.Burn().processLog(hex_log)
+    return processed_log
+
+def process_log(contract, log, topics):
+    log_topic_0 = log['topics'][0]
+    hex_log = convert_dict_to_hexbytes(log.copy())
+    process_function = topics[log_topic_0]
+    processed_log = process_function(contract, hex_log)
+    return processed_log
+
+async def subscribe_to_logs(ws, pair, msg, topics):
     msg["params"][1]["address"] = pair
-    await ws.send(json.dumps(msg))
+    for topic in topics.keys():
+        msg["params"][1]["topics"] = [topic]
+        await ws.send(json.dumps(msg))
 
 async def main():
     conf = dotenv.dotenv_values("./keys/.env")
     ws_endpoint = conf["INFURA_WS_ENDPOINT"]
-    topic = "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822"
+    # Topics for Swaps, Mints, Burns, respectively. Maps to processing function
+    topics = {
+        "0xd78ad95fa46c994b6551d0da85fc275fe613ce37657fb8d5e3d130840159d822": process_swap_log, "0x4c209b5fc8ad50758f13e2e1088ba56a560dff690a1c6fef26394f4c03821c4f": process_mint_log, "0xdccd412f0b1252819cb1fd330b93224ca42612892bb3f4f789976e6d81936496": process_burn_log
+    }
     contract_addr = "0xAE461cA67B15dc8dc81CE7615e0320dA1A9aB8D5"
 
     web3 = Web3(Web3.WebsocketProvider(ws_endpoint))
@@ -72,7 +91,7 @@ async def main():
             "logs",
             {
                 "address": "",
-                "topics": [topic],
+                "topics": [],
             }
         ]
     }
@@ -82,12 +101,18 @@ async def main():
     async with websockets.connect(ws_endpoint) as infura_connection:
         tasks = []
         for pair in pairs:
-            tasks.append(subscribe_to_swap_logs(infura_connection, pair, event_filter_sub_message.copy()))
+            tasks.append(subscribe_to_logs(infura_connection, pair, event_filter_sub_message.copy(), topics))
         await asyncio.gather(*tasks)
-        for _ in range(len(pairs)):
-            print(await infura_connection.recv())
-        async for message in infura_connection:
-            processed_log = process_swap_log(contract_as_parser, json.loads(message)['params']['result'])
+        for _ in range(len(pairs) * 3):
+            msg = await infura_connection.recv()
+            while "result" not in json.loads(msg):
+                msg = await infura_connection.recv()
+            print(msg)
+        print("Finished Subscribing")
+        while True:
+            message = await infura_connection.recv()
+            print(json.dumps(message, indent=4, default=vars))
+            processed_log = process_log(contract_as_parser, json.loads(message)['params']['result'], topics)
             print(json.dumps(processed_log, default=vars, indent=4))
 
 if __name__ == "__main__":
